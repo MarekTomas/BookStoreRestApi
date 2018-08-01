5e5d16a7a8438c140e764bff21ef662a63cc09e5
Marek Tomasik <marek-tomas@wp.pl>
2018-08-01 11:57:58 +0200

    First Commit

---
+++ BookStore/src/main/java/bookstore/config/AppConfig.java
@@ -0,0 +1,13 @@
+package bookstore.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("bookstore")
+public class AppConfig implements WebMvcConfigurer {
+
+}
