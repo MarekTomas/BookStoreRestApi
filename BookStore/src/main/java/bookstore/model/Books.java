fc1b49856eaeddb6f78e41f1e6e02c1e81825011
Marek Tomasik <marek-tomas@wp.pl>
2018-08-01 14:14:27 +0200

    Add configuration and model

---
+++ BookStore/src/main/java/bookstore/model/Books.java
@@ -0,0 +1,64 @@
+package bookstore.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Books {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "title")
+	private String title;
+
+	@Column(name = "author")
+	private String author;
+	
+	@Column(name = "price")
+	private int price;
+	
+	public Books() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	
+}
